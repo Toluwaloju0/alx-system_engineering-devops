13e75463d887d9542f2d7f742af1c68e52150adb
Toluwaloju0 <teecoded1990@gmail.com>
2024-07-21 21:03:59 +0100

    puppet configurations

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,10 @@
 # A file to indtall a package
-package { 'flask':
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
+package { 'Flask':
   ensure   => '2.1.0',
-  name     => 'flask',
   provider => 'pip3',
 }
