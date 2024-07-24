0e8f187bc468d04bf23b00e2a3b969cb3e015466
Toluwaloju0 <teecoded1990@gmail.com>
2024-07-24 09:19:23 +0100

    ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,12 @@
 #A puppet file to configure the client for my ubuntu server
-$str = "Host: 503789-web-01
-	HostName 100:25:22:54
-	PasswordAuthentication no
-	FileIdentity ~/.ssh/school
-  "
-file { 'ssh_config':
-  ensure => 'present',
-  path     => 'ssh_config',
+$str = @("EOF")
+Host *
+    IdentityFile ~/.ssh/school
+    PasswordAuthentication no
+EOF
+
+file { '/etc/ssh/ssh_config':
+  ensure  => present,
   content => $str,
 }
+
