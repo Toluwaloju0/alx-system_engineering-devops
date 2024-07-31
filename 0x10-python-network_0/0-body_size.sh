04161a1ffbe3d3c2e824c2dd0d6d21ab0d5b359d
Toluwaloju0 <teecoded1990@gmail.com>
2024-07-31 20:57:51 +0100

    removed

--- 0x10-python-network_0/0-body_size.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# A script to display the sixlze of a url response
-curl -s --write-out "%{size_download}\n" "$1" | tail -1
--- 0x10-python-network_0/1-body.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# A script to get a uri with  200 response code
-curl -s --retry 0 "$1"
