b3d56201a64301f9312a941b2f18845aa03e4f17
Toluwaloju0 <teecoded1990@gmail.com>
2024-08-14 05:50:55 +0100

    api advanced

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -12,4 +12,12 @@ def recurse(subreddit, hot_list=[]):
     """
 
     url = "http://api.reddit.com/r/{}/top".format(subreddit)
-    reddit = requests.get(
+    reddit = requests.get(url, allow_redirects=False)
+    if reddit.status_code != 200:
+        return None
+    data = reddit.json()
+    try:
+        hot_list.append(data['data']['children'][len(hot_list)]['data']['title'])
+        return recurse(subreddit, hot_list=hot_list)
+    except IndexError:
+        return hot_list
