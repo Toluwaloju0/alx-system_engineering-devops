38756e3d4de6f12f55a9ea989942f481d4ed190b
Toluwaloju0 <teecoded1990@gmail.com>
2024-08-15 23:53:44 +0100

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -18,5 +18,5 @@ def number_of_subscribers(subreddit):
     if reddit.status_code != 200:
         return 0
     data = reddit.json()
-
+    print(data['data']['subscribers'])
     return data['data']['subscribers']
