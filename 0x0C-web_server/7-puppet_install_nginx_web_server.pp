7fe074e09919adaa5853de19a561e5021bc53baa
Toluwaloju0 <teecoded1990@gmail.com>
2024-08-28 13:36:11 +0100

    config

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,11 +1,31 @@
 #A puppet code to install nginx and configure it
 package { 'systemctl':
-	ensure => installed,	
-}
-package { 'nginx':
-	ensure => installed,
+    ensure => installed,
 }
 
-package { 'ufw':
-	ensure => installed,
+package { 'nginx':
+    ensure => installed,
+}
+
+file { '/etc/nginx/sites-available/default':
+    ensure => present,
+}
+
+file {'/var/www/html/index.html':
+    ensure  => file,
+    content => 'Hello World!',
+}
+
+$content = @("EOF")
+\tlocation /redirect_me;
+\t{
+\t\treturn 301 index.html;
+\t}
+EOF
+
+fileline { 'insert_after':
+    path  => '/etc/nginx/sites-available/default',
+    line  => $content,
+    match => 'server_name _;',
+    after => 'server_name _;',
 }
