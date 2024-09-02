c07e0efe5481f13d8d945884ee845ee9411abe3f
Toluwaloju0 <teecoded1990@gmail.com>
2024-09-02 05:32:11 +0100

    api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""A module to query an api"""
+
+import requests
+from sys import argv
+
+
+def get_name(url):
+    """To get the name of the user"""
+    response = requests.get(url)
+    response = response.json()
+    return response['name']
+
+
+def get_task(url):
+    """To get the number of tasks done by the user"""
+    response = requests.get(url)
+    response = response.json()
+    total = 0
+    comp = 0
+    comp_list = []
+    # Get the completed tasks and total number of tasks
+    for a in response:
+        total += 1
+        if a['completed']:
+            comp += 1
+            comp_list.append(a['title'])
+    # Return a tuble containing the completed, total and names of task
+    return comp, total, comp_list
+
+
+if __name__ == '__main__':
+    url_user = f"https://jsonplaceholder.typicode.com/users/{argv[1]}"
+    url_task = f"https://jsonplaceholder.typicode.com/todos?userId={argv[1]}"
+    e_name = get_name(url_user)
+    comp, total, tasks = get_task(url_task)
+
+    # Print the output
+    print(f"Employee {e_name} is done with tasks({comp}/{total}):")
+    for task in tasks:
+        print(f"\t {task}")
